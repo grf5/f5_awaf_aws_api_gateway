603d47823136dcf3a27e71f78e92861ccce85564
Greg Robinson <g.robinson@f5.com>
2021-07-29 16:39:23 -0400

    adding private ips to output

--- experimental/multi-nic/terraform/outputs.tf
+++ experimental/multi-nic/terraform/outputs.tf
@@ -10,6 +10,14 @@ output "JuiceShopAPIAZ2URL" {
   description = "URL to front-end of Juice Shop API in AZ2"
   value = format("http://%s",aws_eip.juiceShopAPIAZ2EIP.public_ip)
 }
+output "JuiceShopAPIAZ1PrivateIP" {
+  description = "the private IP address of the Juice Shop API server in AZ1"
+  value = aws_network_interface.juiceShopAPIAZ1ENI.private_ip
+}
+output "JuiceShopAPIAZ2PrivateIP" {
+  description = "the private IP address of the Juice Shop API server in AZ1"
+  value = aws_network_interface.juiceShopAPIAZ2ENI.private_ip
+}
 output "BIG-IP_AZ1_Mgmt_URL" {
   description = "URL for managing the BIG-IP in AZ1"
   value = format("https://%s/",aws_eip.F5_BIGIP_AZ1EIP_MGMT.public_ip)
